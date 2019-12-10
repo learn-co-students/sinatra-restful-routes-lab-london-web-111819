a53c173ff2b9329a1bde2b8253a249b108ea1cb4
Matthew <matthew@matthews-mbp.home>
2019-12-10 20:56:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,38 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/' do 
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do 
+    recipy = Recipy.create(params)
+  end
+
+  get '/recipes/:id' do
+    @recipy = Recipy.find(params[:id])
+    erb :show 
+  end
+
+  get '/recipes/:id/edit' do 
+    @recipy = Recipy.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do 
+    @recipy = Recipy.find(params[:id])
+    @recipy.update(params[:recipy])
+    redirect "/recipes/#{@recipy.id}"
+  end
+
+  delete '/recipes/:id' do 
+    recipy = Recipy.find(params[:id])
+    recipy.destroy
+    redirect "/recipes"
+  end
 
 end
